744cfca650b836d5900acf105b05a533b9359b8a
isacepifanioo <isacepifanio4@gmail.com>
2022-12-22 20:43:06 -0300

    js

--- aula011/ex009.js
+++ aula011/ex009.js
@@ -1,4 +1,6 @@
-var a = 61
+var a = 63
+console.log(`A velocidade do carro e ${a} km/h`)
 if (a > 60) {
     console.log('Voce passou do limite, e vai receber uma multa')
 }
+console.log('dirija sempre com cinto de segurança')
---
+++ aula011/ex010.js
@@ -0,0 +1,6 @@
+var pais = 'eua'
+if (pais == 'brasil') {
+    console.log('Você e brasileiro')
+} else {
+    console.log('você e estrangeiro')
+}
