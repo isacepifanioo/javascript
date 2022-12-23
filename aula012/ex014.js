ad8c7bf2973f482ee29810125e16959546136409
isacepifanioo <isacepifanio4@gmail.com>
2022-12-23 14:38:45 -0300

    js

---
+++ aula012/ex014.js
@@ -0,0 +1,30 @@
+var sem = new Date()
+var diasem = sem.getDay()
+
+
+switch(diasem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default:
+        console.log ('[Erro] Esse dia da semana, Não existe')
+        break
+}
