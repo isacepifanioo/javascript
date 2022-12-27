da423b6e6b9cb39af73c76554057c80340a531b1
isacepifanioo <isacepifanio4@gmail.com>
2022-12-27 13:14:45 -0300

    js

--- aula012/pratica002/script.js
+++ aula012/pratica002/script.js
@@ -10,12 +10,47 @@ function verificar() {
         var fsex = window.document.getElementsByName('sexo')
         var idade = ano - Number(nasc.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade <= 5) {
+                //bebe
+                img.setAttribute('src', 'mulher/fcrianca.png')
+            } else if(idade >= 6 && idade <= 14) {
+                //criança
+                img.setAttribute('src', 'mulher/fcriancam.png')
+            } else if (idade >= 15 && idade <= 21) {
+                //adolecente
+                img.setAttribute('src', 'mulher/fadolecente.png')
+            } else if (idade >= 22 && idade < 50) {
+                //Mulher
+                img.setAttribute('src', 'mulher/mulher.png')
+            } else if (idade > 50) {
+                //idoso
+                img.setAttribute('src', 'mulher/fidosa.png')
+            }
         } else {
             genero = 'Homem'
+            if (idade >= 0 && idade <= 5) {
+                //bebe
+                img.setAttribute('src', 'homem/mbebe.png')
+            } else if(idade >= 6 && idade <= 14) {
+                //criança
+                img.setAttribute('src', 'homem/crianca.png')
+            } else if (idade >= 15 && idade <= 21) {
+                //adolecente
+                img.setAttribute('src', 'homem/madolecente.png')
+            } else if (idade >= 22 && idade < 50) {
+                //homem
+                img.setAttribute('src', 'homem/homem.png')
+            } else if (idade > 50) {
+                //idoso
+                img.setAttribute('src', 'homem/midoso.png')
+            }
         }
     }
     res.style.textAlign = 'center'
     res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    res.appendChild(img)
 }
