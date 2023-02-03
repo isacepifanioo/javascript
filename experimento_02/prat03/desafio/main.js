445affa70935ecfe414b409725a0a79db2e3c718
isacepifanioo <isacepifanio4@gmail.com>
2023-02-03 19:53:01 -0300

    ㅤ

--- experimento_02/prat03/desafio/main.js
+++ experimento_02/prat03/desafio/main.js
@@ -1,6 +1,12 @@
 var listaDeFilmes = ['https://br.web.img2.acsta.net/pictures/21/12/16/10/26/5839797.jpg', 'https://br.web.img2.acsta.net/pictures/21/10/27/11/49/5430171.jpg', 'https://criticalhits.com.br/wp-content/uploads/2019/03/image.jpg', 'https://sm.ign.com/ign_br/screenshot/default/pokemon-first_32qq.jpg', 'https://www.intoxianime.com/wp-content/uploads/2022/09/fdaie8ex0aaz_es.webp', 'https://animefire.net/img/animes/boku-no-hero-academia-the-movie-2-heroes-rising-large.webp?v=1', 'https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/d48d4a62b0ac6381c87bd040b69b0a89.jpe']
+
+var listaComNomes = ['Dragon ball super hero', 'Jujutsu kaisen', 'Naruto', 'Pokemon', 'nanatsu no taizai', 'Boku no hero', 'Demon slayer']
+document.write('<div class="container_todosFilmes">')
 var i = 0
-while (i < listaDeFilmes.length) {
+while (i < listaDeFilmes.length) { 
+    document.write('<div class="container_filme">')  
     document.write('<img src=' + listaDeFilmes[i] + '>')
+    document.write('<p>' + listaComNomes[i] + '</p>')
+    document.write('</div>')
     i++
 }
