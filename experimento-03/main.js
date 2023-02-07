daef73b04854b3924563ed1a84180e309865e689
isacepifanioo <isacepifanio4@gmail.com>
2023-02-06 22:55:51 -0300

    Update main.js

--- experimento-03/main.js
+++ experimento-03/main.js
@@ -1,7 +1,4 @@
-
 function verficarFilmes() {
-    document.write('div id="cantoDoFilmes"')
     var imagem = window.document.getElementById('imagemDoFilme').value
     document.write('<img src=' + imagem + '>')
-    document.write('</div>')
 }
