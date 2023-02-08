78bfb54d6388cbfe0f1c19f7ee1ac6d47bb9a1e1
isacepifanioo <isacepifanio4@gmail.com>
2023-02-08 20:42:49 -0300

    Create var01.js

---
+++ Variáveis/var01.js
@@ -0,0 +1,9 @@
+let numero = 80 
+if (numero == 80) {
+    console.log('Ok')
+} else if (numero < 80){
+    console.log(`O numero ${numero} esta mais para cima`)
+} else if (numero > 80) {
+    console.log(`O numero ${numero} estar mais a baixo`)
+}
+
