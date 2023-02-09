d322159d3ee0ef5159aaa9cafbdd43416819cc4d
isacepifanioo <isacepifanio4@gmail.com>
2023-02-08 22:09:03 -0300

    Update var01.js

--- Variáveis/var01.js
+++ Variáveis/var01.js
@@ -1,9 +1,12 @@
-let numero = 80 
-if (numero == 80) {
-    console.log('Ok')
-} else if (numero < 80){
-    console.log(`O numero ${numero} esta mais para cima`)
-} else if (numero > 80) {
-    console.log(`O numero ${numero} estar mais a baixo`)
-}
+// var 
+// let
+// const 'não pode altera o valor apos recebe o primeiro'
 
+const nome = prompt('Qual seu nome: ')
+let idade = parseInt(prompt('Quantos nos você fez ou vai fazer esse ano? '))
+let mesNasc = parseInt(prompt('Em que mes você nasceu? '))
+let diaNasc = parseInt(prompt('Em que dia você nasceu? '))
+ 
+const dataDeNasc = 2023 - idade
+console.log(`Olá ${nome}. Seja bem vindo`)
+console.log(`Sua data de nascimento ${diaNasc}/${mesNasc}/${dataDeNasc}`)
