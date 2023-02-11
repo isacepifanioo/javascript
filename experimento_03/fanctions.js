053dce841ca380f79af6d356bafcafce6bef150b
isacepifanioo <isacepifanio4@gmail.com>
2023-02-11 15:09:02 -0300

    ㅤ

---
+++ experimento_03/fanctions.js
@@ -0,0 +1,12 @@
+corSite = 'Azul'
+corLetras = 'Preto'
+corTonalidade = 'azul-claro'
+function exibirCor(cor, Letras, tonalidade) {
+    corSite = cor
+    corLetras = Letras
+    corTonalidade = tonalidade
+}
+
+console.log(corSite, ',', corLetras, ',',  corTonalidade)
+exibirCor('vermelho', 'branco', 'verde-claro')
+console.log(corSite, ',', corLetras, ',',  corTonalidade)
