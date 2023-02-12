653a3905f46fb47ebf8956fc0b6778454d594060
isacepifanioo <isacepifanio4@gmail.com>
2023-02-11 22:37:43 -0300

    Update fatorial.js

--- experimento_03/fatorial.js
+++ experimento_03/fatorial.js
@@ -1,10 +1,14 @@
 output = ''
+let res = 1
 function fatorial(numb) {
-    for (let i = 10;i < numb; i--) {
-        output += i + ' '
+  for (let i = numb;i > 0;i--) {
+    output += i + ''
+    if (i > 1) {
+      res *= i
+      output += ' x '
     }
+  }
 }
 
-
-let resultado = fatorial(1)
-console.log(output)
+let resultado = fatorial(5)
+console.log(output, ' = ', res)
