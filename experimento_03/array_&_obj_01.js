08264452e2e0abaeb4ecf612099b2684bdb201c3
isacepifanioo <isacepifanio4@gmail.com>
2023-02-13 23:51:57 -0300

    ㅤ

---
+++ experimento_03/array_&_obj_01.js
@@ -0,0 +1,44 @@
+let informação = [
+    pessoa = {
+        nome: 'Isac',
+        idade: 17,
+        genero: 'M',
+        email: 'isacepifanio17@gmail.com',
+    },
+    pessoa = {
+        nome: 'Elayne',
+        idade: 29,
+        genero: 'F',
+        email: 'elaynekesia@gmail.com',
+    },
+    pessoa = {
+        nome: 'Helane',
+        idade: 28,
+        genero: 'F',
+        email: 'helanekedima98@gmail.com',
+    },
+    pessoa = {
+        nome: 'Vladia',
+        idade: 54,
+        genero: 'F',
+        email: 'vladiaana89@gmail.com',
+    },
+    pessoa = {
+        nome: 'Joao Vitor',
+        idade: 3,
+        genero: 'M',
+        email: 'vitorjoao0602@gmail.com',
+    }
+]
+console.log('=================================')
+for (let i = 0; i < informação.length; i++) {
+    let unica = informação[i]
+    if (true) {
+        console.log(informação[i])
+        console.log(unica.nome)
+        console.log(unica.idade)
+        console.log(unica.genero)
+        console.log(unica.email)
+        console.log('=================================')
+    }
+}
