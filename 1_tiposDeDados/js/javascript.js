5af931d79e59dc3957fa315ec0b1885a36c574c2
isacepifanioo <isacepifanio4@gmail.com>
2023-02-15 14:32:49 -0300

    ﾠ

---
+++ 1_tiposDeDados/js/javascript.js
@@ -0,0 +1,7 @@
+// number, string, boolean, null e undefined, object;
+// Verificar com => typeOf;
+
+//  possui três Valores simbólicos: 
+// +Infinity == Valores positivo
+// -Infinity == Valores negativo
+//  NaN == Uma operação matematica que não resulta em um numero
