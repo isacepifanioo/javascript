07f8673303076408f407e9d8aef4372a58ded4c6
isacepifanioo <isacepifanio4@gmail.com>
2023-02-15 22:52:23 -0300

    ㅤ

---
+++ 7_tipoDeDados_(=== E !===)_/js/javascript.js
@@ -0,0 +1,19 @@
+let numero = '5'
+
+if (numero === 5) { // comparação === ele vai compara se o dipo de dados e o msm 
+    console.log('Esse numero e o 5')
+};
+
+// "5" => string
+// 5 => number
+// se '5' === 5 => false 
+// se '5' == 5 => true
+
+let numero2 = 10
+
+if (numero2 !== '10') {
+    console.log('Esse numero eo 10')
+}
+
+// se '5' !== 5 => true
+// se 5 != 5 => false
